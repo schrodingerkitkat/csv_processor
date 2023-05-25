121ba088e77c3bd6d417df92b1f12818486c4523
leuc0sia <katherine@madhuri.me>
2023-05-24 23:52:15 -0700

    barebones airflow file

---
+++ dag.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+
+
+
+#import python script
+
+
+#set arguments for dag
+
+
+#instantiate the dag
+
+
+#define the task
+
+
+
+#defint task ordering
+
+
